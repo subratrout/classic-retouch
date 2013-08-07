f864e25941396b727704f30bcb2de34fe3ab3d3f
Subrat Rout <subratnrout@gmail.com>
2013-08-07 16:13:06 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClassicRetouch::Application.config.secret_key_base = 'e371574421e91e1b69c1f10c072a25830c255f08527dbe00ac681197b24f13f71d90a5fa3daf0a8dd7ac21581dcaf5aa6702fa080759a81b2eaa94942acd217b'
